6c794463b9152bdf514ecd79c2ca2f51d87bae3b
youngderrick82 <youngderrick82@gmail.com>
2023-12-07 22:07:51 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,93 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const axios = require("axios");
 const cors = require("cors");
 
+// Initialize your Express app
 const app = express();
+
+// Use CORS - adjust as needed for your deployment environment
+app.use(
+  cors({
+    origin: "http://localhost:3000", // You might need to update this for your production environment
+  })
+);
+
+app.set("trust proxy", true);
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+// API routes
+const TWITCH_CLIENT_ID = process.env.TTV_CLIENT_ID;
+const TWITCH_CLIENT_SECRET = process.env.TTV_CLIENT_SECRET;
+
+let accessToken = null;
+let tokenExpiry = null;
+
+const getToken = async () => {
+  try {
+    const response = await axios.post(
+      "https://id.twitch.tv/oauth2/token",
+      null,
+      {
+        params: {
+          client_id: TWITCH_CLIENT_ID,
+          client_secret: TWITCH_CLIENT_SECRET,
+          grant_type: "client_credentials",
+        },
+      }
+    );
+
+    accessToken = response.data.access_token;
+    const expiresIn = response.data.expires_in;
+    tokenExpiry = Date.now() + expiresIn * 1000; // Convert to milliseconds
+    console.log("Twitch token fetched successfully");
+  } catch (error) {
+    console.error("Error fetching Twitch token:", error);
+    throw error;
+  }
+};
+
+app.get("/get-twitch-token", async (req, res) => {
+  if (!accessToken || Date.now() >= tokenExpiry) {
+    await getToken();
+  }
+
+  res.json({ access_token: accessToken });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
+
+/* require("dotenv").config();
+const express = require("express");
+const path = require("path")
+const axios = require("axios");
+const cors = require("cors");
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+const app = express();
+
 app.use(
   cors({
     origin: "http://localhost:3000",
@@ -54,3 +138,4 @@ app.get("/get-twitch-token", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+ */
